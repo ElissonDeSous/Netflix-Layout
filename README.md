1fc42341bdcb2f5e31b7cb7e14f43bba2bd64ada
ElissonDeSous <elisosndsousa@gmail.com>
2024-06-18 19:45:19 -0300

    atualização react router dom

---
+++ README.md
@@ -0,0 +1,3 @@
+layout da netflix feito em react JS 
+
+![image](https://github.com/ElissonDeSous/Netflix-clone/assets/94570280/43970a3f-5f5f-4ba7-8d31-290c1cfe101a)
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css'
+import {BrowserRouter ,Route ,Routes} from 'react-router-dom'
+//PAGINAS
+import Home from "./pages/home/home"
+import Register from "./pages/register/Register"
+import Login from "./pages/login/Login"
+
+// componentes
+import Topo from "./componentes/header/header"
+import Rodape from './componentes/footer/footer'
+
+
+
+
+function App() {
+
+  return (
+   <BrowserRouter>
+   <Topo/>
+   <Routes>
+      <Route path='/' element = {<Home/>} />
+      <Route path='/login' element = {<Login/>} />
+      <Route path='/registrar' element = {<Register/>} />
+   </Routes>
+   <Rodape/>
+   </BrowserRouter>
+  )
+}
+
+export default App
